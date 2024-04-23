9b74e754b1ee1721835f175b0a3f3e35e4599b1d
Paragvun34 <vladmelnik200192@gmail.com>
2024-04-23 12:12:31 +0300

    Fisrt_commit

---
+++ quality_control/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tasks.models import Task, Project
+
+# Create your models here.
+class BugReport(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        related_name='bug_report',
+        on_delete=models.CASCADE,
+    )
+    task = models.ForeignKey(
+        Task,
+        related_name='bug_report',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+
+    priority = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class FeatureRequest(models.Model):
+    
+    # Кортеж из возможных статусов задачи
+    STATUS_CHOICES = [
+        ('Consideration', 'Рассмотрение'),
+        ('Accepted', 'Принято'),
+        ('Rejection', 'Отклонение'),
+    ]
+    
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        related_name='feature_req',
+        on_delete=models.CASCADE,
+    )
+    task = models.ForeignKey(
+        Task,
+        related_name='feature_req',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='Consideration',
+    )
+
+    priority = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
